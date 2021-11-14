a0d794155d162f7353b8a6f13ee1f5ed8369ace1
Camilo Valenzuela <62782727+Camiloignaval@users.noreply.github.com>
2021-11-13 22:45:37 -0300

    Agregada nota a algoritmos

--- algoritmos/index.js
+++ algoritmos/index.js
@@ -1,9 +1,14 @@
 import { CalculadoraCompleja } from "./CalculadoraCompleja.js"
 import { CalculadoraSimple } from "./CalculadoraSimple.js"
 
+// Porfavor agregar a package.json lo siguiente:
+// {...
+//   "type" : "module"
+//  ...}
+// Quitar para ejecutar programa de bikeShop
+
 console.log('------------------------------------------ \nDemostración Calculo de Stock de Seguridad \n------------------------------------------\n\nTomando 5 valores aleatorios...'
 )
-
 
 
 const lista = [1, 2, 3, 4, 5].map(() => Math.floor(Math.random() * 51))
